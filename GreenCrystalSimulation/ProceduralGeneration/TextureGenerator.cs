e10421754450fb997fe926f60fa76c1f1d1ec5c9
группа <@241-3>
2024-09-16 14:18:14 +0500

    Разрабатывается генератор пиксилей.

---
+++ GreenCrystalSimulation/ProceduralGeneration/TextureGenerator.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GreenCrystalSimulation.ProceduralGeneration
+{
+    public class TextureGenerator
+    {
+        GraphicsDeviceManager graphics;
+        public TextureGenerator()
+        {
+
+        }
+
+        public Texture2D GenaratePixel(Color color)
+        {
+            Color[] colors;
+            Texture2D texture;
+            return texture;
+        }
+        private Color[] FillColors(Color color)
+        {
+            Color[] colors;
+            for (int i = 0; i > 0; i++)
+            {
+                colors[i] = color;
+            }
+            return colors;
+        }
+    }
+}
